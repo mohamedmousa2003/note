a7087d1c44c64faf946d4109d3431004c71a14d9
Mohamed Mousa <mohamedmousa11520031@gmail.com>
2024-09-10 19:27:55 +0300

    note one

---
+++ bin/note.dart
@@ -0,0 +1,10 @@
+
+void main() {
+  // note 1
+  //  When I see the letter “m” in a word, I want to change it to "op" letter
+  // used replaceAll
+  String name ="mohamed";
+  print(name.replaceAll("m", "op"));
+  // run opohaoped
+  // end note 1
+}
